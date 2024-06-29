0211c580d1bd3edc8b4315aa09c9a5665ce9a452
Proscka <proskinaviktoria861@gmail.com>
2024-06-29 18:17:33 +0300

    Add files via upload

---
+++ Операторы if,elif, else Условные конструкции/main.py
@@ -0,0 +1,30 @@
+first="123"
+print(first)
+second="456"
+print(second)
+third="789"
+print(third)
+if first is not second is not third :
+    print (0)
+else:
+    print(0)
+a=42
+print(a)
+b=69
+print(b)
+c=42
+print(c)
+if a is not b is not c:
+else:
+    print(2)
+
+
+
+
+
+
+
+
+
+
+
